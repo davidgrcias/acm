b854b7c4a45e1b401f46bf7472ef690544e0becd
David G Tech <76923507+davidgrcias@users.noreply.github.com>
2024-11-02 14:36:33 +0700

    laravel with filament admin

---
+++ app/Filament/Resources/NewsResource.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\NewsResource\Pages;
+use App\Filament\Resources\NewsResource\RelationManagers;
+use App\Models\News;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ImageColumn;
+use Illuminate\Support\Facades\Storage;
+use Filament\Forms\Components\FileUpload;
+use Filament\Tables\Columns\SelectColumn;
+use Filament\Forms\Components\Select;
+
+class NewsResource extends Resource
+{
+    protected static ?string $model = News::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form->schema([
+            Forms\Components\TextInput::make('title')
+                ->required()
+                ->maxLength(255),
+            Forms\Components\Textarea::make('text')
+                ->required()
+                ->columnSpan(2),
+            Forms\Components\FileUpload::make('cover_image')
+                ->columns(4)
+                ->directory('cover_image_news')
+                ->storeFileNamesIn('original_filename'),
+            Forms\Components\TextInput::make('author')
+                ->required()
+                ->maxLength(255),
+            Select::make('status')
+                ->options([
+                    'draft' => 'Draft',
+                    'reviewing' => 'Reviewing',
+                    'published' => 'Published',
+                ])
+                ->required()
+                ->label('Status'),
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\ImageColumn::make('cover_image')
+                    ->label('Cover Image')
+                    ->disk('public')  // Confirm this is correctly set in filesystems config
+                    ->url(fn($record) => asset('storage/' . $record->cover_image))
+                    ->size(100, 100),
+                TextColumn::make('title')
+                    ->sortable()
+                    ->searchable()
+                    ->label('Title'),
+                TextColumn::make('author')
+                    ->sortable()
+                    ->searchable()
+                    ->label('Author'),
+                TextColumn::make('text')
+                    ->words(25)
+                    ->sortable()
+                    ->searchable()
+                    ->wrap()
+                    ->label('Content'),
+                TextColumn::make('created_at')
+                    ->dateTime() // Format as a date-time column
+                    ->label('Published Date')
+                    ->sortable(),
+                SelectColumn::make('status')
+                    ->options([
+                        'draft' => 'Draft',
+                        'reviewing' => 'Reviewing',
+                        'published' => 'Published',
+                    ])
+                    ->sortable()
+                    ->searchable()
+                    ->label('Status'),
+            ])
+            ->filters([
+                // Add any filters if necessary
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListNews::route('/'),
+            'create' => Pages\CreateNews::route('/create'),
+            'edit' => Pages\EditNews::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ resources/views/filament/widgets/text-credit.blade.php
@@ -0,0 +1,7 @@
+<x-filament::widget>
+    <x-filament::section>
+        <div class="text-center p-6 rounded-lg shadow-md bg-white dark:bg-gray-800">
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200">ARK Care Ministry</h2>
+        </div>
+    </x-filament::section>
+</x-filament-widgets::widget>
