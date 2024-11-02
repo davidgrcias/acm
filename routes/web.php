abc0658bc74639d504fe1951f32fa8d583a8e97c
David G Tech <76923507+davidgrcias@users.noreply.github.com>
2024-11-02 23:42:20 +0700

    fix authentication and admin bugs

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+/* david */
+
+Route::get('/login', function () {
+    return redirect(route('filament.admin.auth.login'));
+})->name('login');
+/* end david */
+
+
 Route::get('/', function () {
     return view('welcome');
 });
