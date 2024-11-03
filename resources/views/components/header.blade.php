297de30f7400574e744ceeba7dcc0d919332d48a
hezki <yehezkielnatanael05@gmail.com>
2024-11-03 20:21:12 +0700

    template buat UI USER

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>{{ $slot }}</title>
+
+    <link href="templateUSER/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="templateUSER/css/bootstrap-icons.css" rel="stylesheet">
+
+    <link href="templateUSER/css/templatemo-kind-heart-charity.css" rel="stylesheet">
+
+</head>
