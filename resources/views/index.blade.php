861c06ab704a29433cff59dd539df9dc3ae457e5
akunjone <johnwitness0@gmail.com>
2024-11-25 18:16:54 +0700

    Rangka program

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,10 +3,10 @@
     <x-slot:title>{{  $title }}</x-slot:title>
     <style>
         .container-fotowelcome {
-            background-image: url('/images/home.jpg');
+            /*background-image: url('../Resources/');*/
+            background-color: green;
             width: 90%;
             padding: 90px;
-            background-color:green; /*masih sementara, nanti ganti fotonya jd background*/
             border-radius:50px;
             margin-top: 30px;
             margin-bottom: 30px;
@@ -50,12 +50,42 @@
             <a href="/about" class="tombol-about">About Us</a>
         </div>
         <div class="quotes" align="center">
-            <p>True Evangelical faith, cannot lie dormant, it clothes the naked, it feeds the hungry
+            <p style="color:black;">True Evangelical faith, cannot lie dormant, it clothes the naked, it feeds the hungry
                 it comforts the sorrowful, it shelters the destitute, it serves those that harm, it binds 
                 up that which is wounded, it has become all things to all creatures
                 <br/><br/>
-                Menno Simmons<br/>
             </p>
+            <p>Menno Simmons<br/></p>
         </div>
+        <!--yg carousel sampe join us dah sama nanda-->
+        <div class="ourprogram">
+            <h3>Our Program</h3>
+            <h6>We help those in need</h6>
+            <div class="beritaourprogram">
+                <div id="newsCarousel" class="carousel slide" data-bs-interval="false">
+                    <div class="carousel-inner">
+                        @php $i = 0; @endphp
+                        @foreach ($fillables as $fillable)
+                            <div class="carousel-item {{ $i == 0 ? 'active' : '' }}">
+                                <div class="news-content">
+                                    <img src="{{ asset($newsItem->photo) }}" class="d-block w-100" alt="{{ $newsItem->title }}">
+                                    <h4>{{ $newsItem->title }}</h4>
+                                    <p>{{ $newsItem->description }}</p>
+                                </div>
+                            </div>
+                            @php $i++; @endphp
+                        @endforeach
+                    </div>
+                    <a class="carousel-control-prev" href="#newsCarousel" role="button" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#newsCarousel" role="button" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </a>
+                </div>
+            </div>
+        </div>  
     </div>
 </x-layout>
