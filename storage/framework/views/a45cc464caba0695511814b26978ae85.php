05714989b13c65f106946c12e47a9fb638d14a51
Ric4j4 <richardwijaya31@gmail.com>
2024-12-06 01:53:14 +0700

    a
    a

---
+++ storage/framework/views/a45cc464caba0695511814b26978ae85.php
@@ -0,0 +1,5 @@
+<div <?php echo e($attributes->class(['fi-dropdown-list p-1'])); ?>>
+    <?php echo e($slot); ?>
+
+</div>
+<?php /**PATH C:\Users\dedec\OneDrive\Gambar\Dokumen\GitHub\acm\resources\views/vendor/filament/components/dropdown/list/index.blade.php ENDPATH**/ ?>
