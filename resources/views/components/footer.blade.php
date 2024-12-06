d6991117006317f0cc5d3b28cd7ded8640981ded
MichKyleEl <164639246+MichKyleEl@users.noreply.github.com>
2024-12-06 12:15:45 +0700

    Fix footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,72 +1,68 @@
 <footer class="site-footer">
     <div class="container">
-        <div class="row">
-            <div class="col-lg-4 col-md-6 col-12 mb-4" style="color:white;">
-                <h5 class="site-footer-title mb-3">{{ $views->contact_title }}</h5>
+        <div class="row align-items-center">
 
-                <ul class="footer-menu">
-                    <li class="footer-menu-item"><a href="#" class="footer-menu-link">Our Story</a></li>
-                    <li class="footer-menu-item"><a href="#" class="footer-menu-link">Newsroom</a></li>
-                    <li class="footer-menu-item"><a href="#" class="footer-menu-link">Causes</a></li>
-                    <li class="footer-menu-item"><a href="#" class="footer-menu-link">Become a volunteer</a></li>
-                    <li class="footer-menu-item"><a href="#" class="footer-menu-link">Partner with us</a></li>
+            <!-- Contact Section -->
+            <div class="col-lg-6 col-md-6 col-12 mb-4">
+                <h5 class="text-white mx-auto px-3 py-6 text-center text-md-start">Contact</h5>
+                <ul class="container flex flex-column pt-4 pb-4 px-3 text-center text-md-start">
+                    <li class="text-white footer-menu-item pt-1 pb-1">ARK Care Ministry</li>
+                    <li class="text-white footer-menu-item pt-1 pb-1">096-907-097</li>
+                    <li class="text-white footer-menu-item pt-1 pb-1">arkcareministry@acm.org.id</li>
                 </ul>
             </div>
 
-            <div class="col-lg-4 col-md-6 col-12 mx-auto">
-                <h5 class="site-footer-title mb-3">Contact Information</h5>
-
-                <p class="text-white d-flex mb-2">
-                    <i class="bi-telephone me-2"></i>
-                    <a href="tel: 305-240-9671" class="site-footer-link">{{ $views->contact_phone_number }}</a>
-                </p>
-
-                <p class="text-white d-flex">
-                    <i class="bi-envelope me-2"></i>
-                    <a href="mailto:{{ $views->contact_email }}" class="site-footer-link">{{ $views->contact_email }}</a>
-                </p>
-
-                <p class="text-white d-flex mt-3">
-                    <i class="bi-geo-alt me-2"></i>
-                    {{ $views->contact_name }} 
-                </p>
-
-                <a href="#" class="custom-btn btn mt-3">Get Direction</a>
+            <!-- Logo Section -->
+            <div class="col-lg-6 col-md-6 col-12 text-center text-md-end mb-4">
+                <img src="templateUSER/images/logoACM.png" alt="ARK Care Ministry"
+                    class="object-cover w-75 h-auto">
             </div>
+
         </div>
     </div>
 
-    <div class="site-footer-bottom">
+    <hr class="my-3" style="width: 95%; border: none; background-color: white; height: 1px; margin: 0 auto;">
+
+    <div class="pt-3 pb-3">
         <div class="container">
-            <div class="row">
-                <div class="col-lg-6 col-md-7 col-12">
-                    <p class="copyright-text mb-0">
-                        Copyright © 2036 <a href="#">Kind Heart</a> Charity Org. Design: <a href="https://templatemo.com" target="_blank">TemplateMo</a><br>Distribution: <a href="https://themewagon.com">ThemeWagon</a>
-                    </p>
+            <div class="row align-items-center">
+
+                <!-- Copyright Text -->
+                <div class="col-lg-6 col-md-6 col-12 text-center text-md-start mb-3 mb-md-0">
+                    <p class="copyright-text mb-0" style="padding-left: 1rem;">© 2024 Ark Care Ministry. All rights reserved.</p>
                 </div>
 
-                <div class="col-lg-6 col-md-5 col-12 d-flex justify-content-center align-items-center mx-auto">
-                    <ul class="social-icon">
-                        <li class="social-icon-item"><a href="#" class="social-icon-link bi-twitter"></a></li>
-                        <li class="social-icon-item"><a href="#" class="social-icon-link bi-facebook"></a></li>
-                        <li class="social-icon-item"><a href="#" class="social-icon-link bi-instagram"></a></li>
-                        <li class="social-icon-item"><a href="#" class="social-icon-link bi-linkedin"></a></li>
-                        <li class="social-icon-item"><a href="https://youtube.com/templatemo" class="social-icon-link bi-youtube"></a></li>
+                <!-- Social Icons -->
+                <div class="col-lg-6 col-md-6 col-12 text-center text-md-end">
+                    <ul class="social-icon d-flex justify-content-center justify-content-md-end gap-3" style="padding-right: 1rem;">
+                        <li class="social-icon-item px-1">
+                            <a href="#" class="social-icon-link bi-instagram px"></a>
+                        </li>
+                        <li class="social-icon-item px-1">
+                            <a href="#" class="social-icon-link bi-whatsapp"></a>
+                        </li>
+                        <li class="social-icon-item px-1">
+                            <a href="#" class="social-icon-link bi-line"></a>
+                        </li>
                     </ul>
                 </div>
+
             </div>
         </div>
     </div>
 </footer>
 
 
+
+
+
 <!-- JAVASCRIPT FILES -->
-<script src="{{ asset('templateUSER/js/jquery.min.js') }}"></script>
-<script src="{{ asset('templateUSER/js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('templateUSER/js/jquery.sticky.js') }}"></script>
-<script src="{{ asset('templateUSER/js/click-scroll.js') }}"></script>
-<script src="{{ asset('templateUSER/js/counter.js') }}"></script>
-<script src="{{ asset('templateUSER/js/custom.js') }}"></script>
+<script src="templateUSER/js/jquery.min.js"></script>
+<script src="templateUSER/js/bootstrap.min.js"></script>
+<script src="templateUSER/js/jquery.sticky.js"></script>
+<script src="templateUSER/js/click-scroll.js"></script>
+<script src="templateUSER/js/counter.js"></script>
+<script src="templateUSER/js/custom.js"></script>
 
 </body>
 
